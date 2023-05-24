25b249dd8c4809ed4735e57e6dfc72d6a67827af
Ing.w <william.esteban@campusucc.edu.com>
2023-05-23 19:02:44 -0500

    [Actualizacion]

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -17,8 +17,8 @@ const Footer = () => {
 
      <section className="flex justify-center sm:justify-end flex-wrap items-center  gap-6 m-6 mt-20 sm:pt-20 text-white text-lg">
        <img className=" ml-8  w-5 sm:w-10" src={ws} alt="" />
-       <span className='mr-8 sm:'>WhatsApp: +573215098953</span>
-       <img className="w-5 sm:w-10" src={outlook} alt="" />
+       <span className='mr-8 text-md sm:'>WhatsApp: +573215098953</span>
+       <img className="w-5 text-md sm:w-10" src={outlook} alt="" />
        <span>Email: <a href="mailto:sr.w@hotmail.com">sr.w@hotmail.com</a></span>
        <span className="text-center ml-0 sm:ml-10 ">
          © William David Esteban Mora - 2023 Todos los derechos reservados
